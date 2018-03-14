b1b002bc3df0c7321479a57c859f380b57e4fb26
Ayeni Blessing <ayeniblessing101@gmail.com>
2018-03-14 16:25:48 +0100

    initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test recipe',
+      'https://photos.smugmug.com/Thai-Recipes-2017/i-cbT2RpB/0/f743dc95/X3/thai-cashew-chicken-recipe-30-X3.jpg'
+    ),
+    new Recipe(
+      'Chinese Recipe',
+      'This is simply a test recipe',
+      'https://cdn.pixabay.com/photo/2017/10/13/19/00/potato-casserole-2848605_1280.jpg'
+    )
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+}
